714c1059855136cac5942fb76f442a092a0202e5
Chris Coppock <chrisc@redravensolutions.com>
2020-10-07 13:01:17 -0400

    maybe this will work..

--- site/src/components/footer.js
+++ site/src/components/footer.js
@@ -1,6 +1,29 @@
 import React from "react";
 import { Link } from "gatsby";
 
+export default function Header() {
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return {
+    data: {
+      site: {
+        siteMetadata: {
+          description: "we break react configs for fun",
+          title: "newReactConf",
+        },
+      },
+    },
+    extensions: {},
+  };
+}
 export default function footer() {
   return (
     <div>

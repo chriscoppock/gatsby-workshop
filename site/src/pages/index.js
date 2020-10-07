186bac54481d00d8ba43b86812d6445fedbb1da5
Chris Coppock <chrisc@redravensolutions.com>
2020-10-07 14:38:00 -0400

    mucking through changes for the speakers

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import sketchnoting from "../images/taking-notes.svg";
+import TalkCard from "../components/TalkCard";
 
 const Hero = () => (
   <div className="text-center">
